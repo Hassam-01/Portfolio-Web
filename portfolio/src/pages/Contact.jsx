3fa3fb6833868162f5bc90bdb53b5d8a55d226ca
hassam-01 <ali.hassam1@outlook.com>
2024-07-11 13:47:44 +0500

    emailjs implemented

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,31 +1,49 @@
 import { useDispatch, useSelector } from "react-redux";
 import connect from "../assets/connect.svg";
 import connectlight from "../assets/contactlight.svg";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { updateEmailBody } from "../store/emailBodySlice";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
   const darkMode = useSelector((state) => state.darkMode.darkMode);
   const dispatch = useDispatch();
-  const formdata = useSelector(state => state.emailBody.emailBody);
+  const form = useRef();
 
   const [formState, setFormState] = useState({
     email: "",
-    firstName: "",
     lastName: "",
+    from_name: "",
+    phone: "",
     message: ""
   });
 
-  const config = {
-    Username: 'portfolicontact@yopmail.com',
-    Password: '105F2B5B0A8F313936CC13E6FFA90CC7B473',
-    Host: 'smtp.elasticemail.com',
-    Port: 2525,
-    To: 'portfolicontact@yopmail.com',
-    From: formState.email,
-    Subject: "This is the subject",
-    Body: formState.message
-  }
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // const phoneRegex = /^\d{10}$/; // Assumes 10-digit phone numbers
+
+    if (!formState.email || !emailRegex.test(formState.email)) {
+      newErrors.email = "Please enter a valid email address.";
+    }
+
+    if (!formState.from_name) {
+      newErrors.from_name = "First name is required.";
+    }
+
+    if (!formState.lastName) {
+      newErrors.lastName = "Last name is required.";
+    }
+
+    if (!formState.message) {
+      newErrors.message = "Message is required.";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -33,25 +51,41 @@ function Contact() {
       ...formState,
       [name]: value
     });
+    dispatch(updateEmailBody({...formState, [name]:value}));
   }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(updateEmailBody(formState));
-    console.log(formdata)
-    if (window.Email) {
-      window.Email.send(config).then(()=> alert("Success!"))
+
+    if (!validateForm()) {
+      return;
     }
+    console.log("here inn")
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID, 
+        import.meta.env.VITE_TEMPLATE_ID, 
+        form.current, 
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', {error});
+        },
+      );
   }
 
   return (
     <div>
-      <div className="flex items-center justify-center mt-4 ">
+      <div className="flex items-center justify-center mt-4">
         <div className="min-w-2xl max-w-5xl w-full bg-gray-400 dark:bg-gray-800 bg-opacity-60 dark:text-white rounded-lg shadow-md p-8 md:flex md:space-x-8">
           <div className="md:w-1/2">
             <h2 className="text-3xl font-bold mb-4">Lets connect constellations</h2>
             <p className="mb-8">Lets align our constellations! Reach out and let the magic of collaboration illuminate our skies.</p>
-            <form className="space-y-4" onSubmit={submitHandler}>
+            <form ref={form} className="space-y-4" onSubmit={submitHandler}>
               <div className="flex space-x-4">
                 <input
                   type="text"
@@ -59,39 +93,45 @@ function Contact() {
                   placeholder="Last Name"
                   value={formState.lastName}
                   onChange={handleChange}
-                  className="w-1/2 p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500"
+                  className={`w-1/2 p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500 ${errors.lastName ? 'border-red-500' : ''}`}
                 />
                 <input
                   type="text"
-                  name="firstName"
+                  name="from_name"
                   placeholder="First Name"
-                  value={formState.firstName}
+                  value={formState.from_name}
                   onChange={handleChange}
-                  className="w-1/2 p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500"
+                  className={`w-1/2 p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500 ${errors.firstName ? 'border-red-500' : ''}`}
                 />
               </div>
+              
               <input
                 type="email"
                 name="email"
                 placeholder="Email"
                 value={formState.email}
                 onChange={handleChange}
-                className="w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500"
+                className={`w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500 ${errors.email ? 'border-red-500' : ''}`}
               />
+              {errors.email && <p className="text-red-500">{errors.email}</p>}
               <input
                 type="tel"
                 name="phone"
                 placeholder="Phone Number"
-                className="w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500"
+                value={formState.phone}
+                onChange={handleChange}
+                className={`w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500 ${errors.phone ? 'border-red-500' : ''}`}
               />
+              {errors.phone && <p className="text-red-500">{errors.phone}</p>}
               <textarea
                 name="message"
                 placeholder="Message"
                 value={formState.message}
                 onChange={handleChange}
-                className="w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500"
+                className={`w-full p-2 rounded bg-gray-300 dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800 dark:focus:ring-purple-500 ${errors.message ? 'border-red-500' : ''}`}
                 rows="4"
               ></textarea>
+              {errors.message && <p className="text-red-500">{errors.message}</p>}
               <button
                 type="submit"
                 className="w-full p-2 rounded text-white dark:bg-purple-600 bg-gray-500 dark:hover:bg-purple-700 hover:bg-gray-700 focus:outline-none focus:ring-2 dark:focus:ring-purple-500 focus:ring-gray-600"
@@ -120,4 +160,4 @@ function Contact() {
   )
 }
 
-export default Contact
+export default Contact;
--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [react()],
+    server: {
+      host : true
+    }
   }
 })
